fe7b13837f76964ab511611bb0c8c72c85a336a0
realrlgus <realrlgus@naver.com>
2020-02-25 23:25:02 +0900

    test

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Links
+} from "react-router-dom";
 import Home from "Routes/Home";
 
 export default () => (
