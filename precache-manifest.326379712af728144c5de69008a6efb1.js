27a20080cf4272670327fbf1f2e8306b96ca4fe5
realrlgus <realrlgus@naver.com>
2020-02-27 04:03:33 +0900

    Updates

---
+++ precache-manifest.326379712af728144c5de69008a6efb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ca2cf4a5808c73722eb6e7b543e778",
+    "url": "/crawling-e-commerce/index.html"
+  },
+  {
+    "revision": "731c4492b34a257553bb",
+    "url": "/crawling-e-commerce/static/js/2.df4ae4aa.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/crawling-e-commerce/static/js/2.df4ae4aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0801b2bcd27c10de6a92",
+    "url": "/crawling-e-commerce/static/js/main.d8f1354f.chunk.js"
+  },
+  {
+    "revision": "3022adc607f59873fca1",
+    "url": "/crawling-e-commerce/static/js/runtime-main.444164cf.js"
+  }
+]);
